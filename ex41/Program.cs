b5db0c5d617baa0ade57b811438236b2045a4ff2
Sergei <ivanovsv90@mail.ru>
2022-09-20 00:50:43 +0500

    init

---
+++ ex41/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] array = InputNumbers();
+PrintArray(array);
+System.Console.WriteLine($"количество введенных положительных чисел = {SearchPositive(array)}");
+
+int[] InputNumbers()
+{
+    System.Console.WriteLine("Введите числа через пробел");
+    string[] parts = Console.ReadLine()!.Split(' ');
+    int[] numbers = new int[parts.Length];
+    for (int i = 0; i < parts.Length; i++)
+        numbers[i] = int.Parse(parts[i]);
+    return numbers;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i]);
+        if (i < arr.Length - 1)
+        {
+            System.Console.Write(", ");
+        }
+    }
+    System.Console.WriteLine();
+}
+
+int SearchPositive(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
---
+++ ex43/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+System.Console.WriteLine(
+    СalculationСoordinate(
+        InputNumbers()));
+
+
+double[] InputNumbers()
+{
+    Console.WriteLine("Введите b1, k1, b2 и k2, значения вводятся через пробел");
+    string[] parts = Console.ReadLine()!.Split(' ');
+    double[] numbers = new double[parts.Length];
+    for (int i = 0; i < parts.Length; i++)
+        numbers[i] = double.Parse(parts[i]);
+    return numbers;
+}
+
+string СalculationСoordinate(double[] arr)
+{
+    double b1 = arr[0];
+    double k1 = arr[1];
+    double b2 = arr[2];
+    double k2 = arr[3];
+
+    if (k1 == k2 && b1 == b2)
+    {
+        return "прямые совпадают. все точки пересекаются";
+    }
+    else if (k1 == k2)
+    {
+        return "Прямые параллельны, нет точки пересечения";
+    }
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k2 * x + b2;
+        return $"x = {x}, y = {y}";
+    }
+}
+
